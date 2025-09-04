8abde331cf23a1f396385aa2e9f872acc71cc9fa
Marco Belladelli <marcobladel@gmail.com>
2025-09-04 11:18:31 +0200

    fixup! HHH-19750 Add test for issue

--- hibernate-core/src/test/java/org/hibernate/orm/test/jpa/query/CriteriaUpdateWithParametersTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jpa/query/CriteriaUpdateWithParametersTest.java
@@ -4,100 +4,113 @@
  */
 package org.hibernate.orm.test.jpa.query;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaUpdate;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.ParameterExpression;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.metamodel.EntityType;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
 import org.hibernate.testing.orm.junit.Jpa;
 import org.junit.jupiter.api.Test;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Query;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaUpdate;
-import jakarta.persistence.criteria.ParameterExpression;
-import jakarta.persistence.criteria.Root;
-import jakarta.persistence.metamodel.EntityType;
 
-
-@Jpa(
-		annotatedClasses = {
-				CriteriaUpdateWithParametersTest.Person.class,
-				CriteriaUpdateWithParametersTest.Process.class
-		}
-)
+@Jpa(annotatedClasses = {
+		CriteriaUpdateWithParametersTest.Person.class,
+		CriteriaUpdateWithParametersTest.Process.class
+})
 public class CriteriaUpdateWithParametersTest {
 
 	@Test
 	public void testCriteriaUpdate(EntityManagerFactoryScope scope) {
-		scope.inTransaction(
-				entityManager -> {
-					final CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-					final CriteriaUpdate<Person> criteriaUpdate = criteriaBuilder.createCriteriaUpdate( Person.class );
-					final Root<Person> root = criteriaUpdate.from( Person.class );
+		scope.inTransaction( entityManager -> {
+			final CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+			final CriteriaUpdate<Person> criteriaUpdate = criteriaBuilder.createCriteriaUpdate( Person.class );
+			final Root<Person> root = criteriaUpdate.from( Person.class );
 
-					final ParameterExpression<Integer> intValueParameter = criteriaBuilder.parameter( Integer.class );
-					final ParameterExpression<String> stringValueParameter = criteriaBuilder.parameter( String.class );
+			final ParameterExpression<Integer> intValueParameter = criteriaBuilder.parameter( Integer.class );
+			final ParameterExpression<String> stringValueParameter = criteriaBuilder.parameter( String.class );
 
-					final EntityType<Person> personEntityType = entityManager.getMetamodel().entity( Person.class );
+			final EntityType<Person> personEntityType = entityManager.getMetamodel().entity( Person.class );
 
-					criteriaUpdate.set(
-							root.get( personEntityType.getSingularAttribute( "age", Integer.class ) ),
-							intValueParameter
-					);
-					criteriaUpdate.where( criteriaBuilder.equal(
-							root.get( personEntityType.getSingularAttribute( "name", String.class ) ),
-							stringValueParameter
-					) );
+			criteriaUpdate.set( root.get( personEntityType.getSingularAttribute( "age", Integer.class ) ),
+					intValueParameter );
+			criteriaUpdate.where(
+					criteriaBuilder.equal( root.get( personEntityType.getSingularAttribute( "name", String.class ) ),
+							stringValueParameter ) );
 
-					final Query query = entityManager.createQuery( criteriaUpdate );
-					query.setParameter( intValueParameter, 9 );
-					query.setParameter( stringValueParameter, "Luigi" );
+			final Query query = entityManager.createQuery( criteriaUpdate );
+			query.setParameter( intValueParameter, 9 );
+			query.setParameter( stringValueParameter, "Luigi" );
 
-					query.executeUpdate();
-				}
-		);
+			query.executeUpdate();
+		} );
 	}
 
 	@Test
 	public void testCriteriaUpdate2(EntityManagerFactoryScope scope) {
-		scope.inTransaction(
-				entityManager -> {
-					final CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-					final CriteriaUpdate<Person> criteriaUpdate = criteriaBuilder.createCriteriaUpdate( Person.class );
-					final Root<Person> root = criteriaUpdate.from( Person.class );
+		scope.inTransaction( entityManager -> {
+			final CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+			final CriteriaUpdate<Person> criteriaUpdate = criteriaBuilder.createCriteriaUpdate( Person.class );
+			final Root<Person> root = criteriaUpdate.from( Person.class );
 
-					final ParameterExpression<Integer> intValueParameter = criteriaBuilder.parameter( Integer.class );
-					final ParameterExpression<String> stringValueParameter = criteriaBuilder.parameter( String.class );
+			final ParameterExpression<Integer> intValueParameter = criteriaBuilder.parameter( Integer.class );
+			final ParameterExpression<String> stringValueParameter = criteriaBuilder.parameter( String.class );
 
-					criteriaUpdate.set( "age", intValueParameter );
-					criteriaUpdate.where( criteriaBuilder.equal( root.get( "name" ), stringValueParameter ) );
+			criteriaUpdate.set( "age", intValueParameter );
+			criteriaUpdate.where( criteriaBuilder.equal( root.get( "name" ), stringValueParameter ) );
 
-					final Query query = entityManager.createQuery( criteriaUpdate );
-					query.setParameter( intValueParameter, 9 );
-					query.setParameter( stringValueParameter, "Luigi" );
+			final Query query = entityManager.createQuery( criteriaUpdate );
+			query.setParameter( intValueParameter, 9 );
+			query.setParameter( stringValueParameter, "Luigi" );
 
-					query.executeUpdate();
-				}
-		);
+			query.executeUpdate();
+		} );
 	}
 
 	@Test
 	public void testCriteriaUpdate3(EntityManagerFactoryScope scope) {
 		scope.inTransaction( em -> {
-			CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-			CriteriaUpdate<Process> createCriteriaUpdate = criteriaBuilder.createCriteriaUpdate(
-					Process.class );
-			Root<Process> root = createCriteriaUpdate.from( Process.class );
-			createCriteriaUpdate.set( root.get( "name" ), (Object) null );
-			createCriteriaUpdate.set( root.get( "payload" ), (Object) null );
-			em.createQuery( createCriteriaUpdate ).executeUpdate();
+			// test separate value-bind parameters
+			final CriteriaBuilder cb = em.getCriteriaBuilder();
+			final CriteriaUpdate<Process> cu = cb.createCriteriaUpdate( Process.class );
+			final Root<Process> root = cu.from( Process.class );
+			cu.set( root.get( "name" ), (Object) null );
+			cu.set( root.get( "payload" ), (Object) null );
+			em.createQuery( cu ).executeUpdate();
 		} );
+
+		scope.inTransaction( em -> {
+			// test with the same cb.value( null ) parameter instance
+			final HibernateCriteriaBuilder cb = (HibernateCriteriaBuilder) em.getCriteriaBuilder();
+			final CriteriaUpdate<Process> cu = cb.createCriteriaUpdate( Process.class );
+			final Root<Process> root = cu.from( Process.class );
+			final Expression<Object> nullValue = cb.value( null );
+			// a bit unfortunate, but we need to cast here to prevent ambiguous method references
+			final Path<String> name = root.get( "name" );
+			final Path<byte[]> payload = root.get( "payload" );
+			final Expression<? extends String> nullString = cast( nullValue );
+			final Expression<? extends byte[]> nullBytes = cast( nullValue );
+			cu.set( name, nullString );
+			cu.set( payload, nullBytes );
+			em.createQuery( cu ).executeUpdate();
+		} );
+	}
+
+	private static <X> Expression<? extends X> cast(Expression<?> expression) {
+		//noinspection unchecked
+		return (Expression<? extends X>) expression;
 	}
 
 	@Entity(name = "Person")
 	public static class Person {
-
 		@Id
 		private String id;
 
@@ -123,7 +136,6 @@ public class CriteriaUpdateWithParametersTest {
 
 	@Entity
 	public static class Process {
-
 		@Id
 		@GeneratedValue
 		private Long id;
@@ -134,6 +146,5 @@ public class CriteriaUpdateWithParametersTest {
 
 		@Lob
 		private byte[] payload;
-
 	}
 }
